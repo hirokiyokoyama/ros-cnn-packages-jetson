f81ae8e20e0ee2ee51b4db5fc2259bd96272a303
Hiroki Yokoyama <h-yokoyama@craft-server.co.jp>
2019-04-23 18:11:33 +0900

    update openpose

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 docker build -t ros-cnn-packages-jetson-common common
 
-docker build -t ros-cnn-packages-jetson-yolo yolo
-docker run --name=ros-cnn-packages-jetson-yolo-tmp ros-cnn-packages-jetson-yolo
-docker commit ros-cnn-packages-jetson-yolo-tmp ros-cnn-packages-jetson-yolo
-docker rm ros-cnn-packages-jetson-yolo-tmp
+docker build --no-cache -t ros-cnn-packages-jetson-yolo yolo
+#docker run --name=ros-cnn-packages-jetson-yolo-tmp ros-cnn-packages-jetson-yolo
+#docker commit ros-cnn-packages-jetson-yolo-tmp ros-cnn-packages-jetson-yolo
+#docker rm ros-cnn-packages-jetson-yolo-tmp
+
+docker build --no-cache -t ros-cnn-packages-jetson-openpose openpose
 
