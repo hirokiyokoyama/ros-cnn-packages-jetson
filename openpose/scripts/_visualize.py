aeedf11d194e117a70311133848dec87c6017f0b
Hiroki Yokoyama <h-yokoyama@craft-server.co.jp>
2019-05-14 18:33:46 +0900

    add level1 topic

--- openpose/scripts/_visualize.py
+++ openpose/scripts/_visualize.py
@@ -13,17 +13,40 @@ from nets import non_maximum_suppression, connect_parts
 from labels import POSE_BODY_25_L2, POSE_BODY_25_L1
 
 bridge = CvBridge()
-image = None
+xavier_image = None
+tx2_image = None
 limbs = [(POSE_BODY_25_L2.index(p), POSE_BODY_25_L2.index(q)) \
          for p, q in POSE_BODY_25_L1]
 
 def sizeof_sparse_tensor(msg):
-    size = 14 #width to max_value
-    size += len(msg.x_indices)
-    size += len(msg.y_indices)
-    size += len(msg.channel_indices)
-    size += len(msg.quantized_values)
-    return size
+  size = 14 #width to max_value
+  size += len(msg.x_indices)
+  size += len(msg.y_indices)
+  size += len(msg.channel_indices)
+  size += len(msg.quantized_values)
+  return size
+
+def extract_people(heat_map, affinity):
+  keypoints = non_maximum_suppression(
+      np.expand_dims(heat_map[:,:,:-1], 0), threshold=0.15)
+  persons = connect_parts(affinity, keypoints[:,1:], limbs,
+                          line_division=15, threshold=0.08)
+  persons = [ { k: ((keypoints[v][2]+0.5)/heat_map.shape[1],
+                    (keypoints[v][1]+0.5)/heat_map.shape[0]) \
+                for k,v in p.items() } for p in persons ]
+  return persons
+
+def draw_people(image, people):
+  for person in people:
+    for k, v in person.items():
+      v = (int(v[0]*image.shape[1]), int(v[1]*image.shape[0]))
+      person[k] = v
+      cv2.circle(image, v, 3, (0, 0, 255), 1)
+    for p1, p2 in limbs:
+      if p1 in person and p2 in person:
+        cv2.line(image,
+                 person[p1], person[p2],
+                 (0, 255, 0), 2)
 
 def callback(image_msg):
     try:
@@ -57,35 +80,34 @@ def callback(image_msg):
     t2 = rospy.Time.now()
     affinity = decode_sparse_tensor(msg2.output_tensors[0])
     heat_map = decode_sparse_tensor(msg2.output_tensors[1])
-    keypoints = non_maximum_suppression(
-        np.expand_dims(heat_map[:,:,:-1], 0), threshold=0.15)
-    sx = image_msg.width/float(heat_map.shape[1])
-    sy = image_msg.height/float(heat_map.shape[0])
-    persons = connect_parts(affinity, keypoints[:,1:], limbs,
-                            line_division=15, threshold=0.08)
-    persons = [ { k: (int(keypoints[v][2]*sx+sx/2),
-                      int(keypoints[v][1]*sy+sy/2)) \
-                  for k,v in p.items() } for p in persons ]
+    people = extract_people(heat_map, affinity)
     t3 = rospy.Time.now()
-
+    
     sizes = list(map(sizeof_sparse_tensor, msg1.output_tensors))
     print("Sizes: {} bytes\nTotal: {} bytes".format(sizes, sum(sizes)))
     print("Image to stage{0}: {1} sec\nStage{0} to detection: {2} sec\nPost processing: {3} sec\nTotal: {4} sec" \
           .format(stage, (t1-t0).to_sec(), (t2-t1).to_sec(),
                   (t3-t2).to_sec(), (t3-t0).to_sec()))
-    print("Found {} people.".format(len(persons)))
+    print("Found {} people.".format(len(people)))
+
+    global xavier_image, tx2_image
+
+    _xavier_image = cv_image.copy()
+    draw_people(_xavier_image, people)
+    xavier_image = _xavier_image
     
-    for person in persons:
-        for k, v in person.items():
-            cv2.circle(cv_image, v, 3, (0, 0, 255), 1)
-        for p1, p2 in limbs:
-            if p1 in person and p2 in person:
-                cv2.line(cv_image,
-                         person[p1], person[p2],
-                         (0, 255, 0), 2)
-    
-    global image
-    image = cv_image
+    #
+    msg1.output_tensors[0].name = 'stage4_L1'
+    msg3 = compute_openpose_xavier(
+        input_tensors=msg1.output_tensors,
+        queries=['stage1_L2'],
+        thresholds=[0.1, 0.1])
+    affinity = decode_sparse_tensor(msg1.output_tensors[0])
+    heat_map = decode_sparse_tensor(msg3.output_tensors[0])
+    people = extract_people(heat_map, affinity)
+    _tx2_image = cv_image
+    draw_people(_tx2_image, people)
+    tx2_image = _tx2_image
 
 if __name__ == '__main__':
     rospy.init_node('visualize_openpose')
@@ -101,6 +123,8 @@ if __name__ == '__main__':
 
     image_sub = rospy.Subscriber('image', Image, callback)
     while not rospy.is_shutdown():
-        if image is not None:
-            cv2.imshow('OpenPose visualization', image)
+        if xavier_image is not None:
+            cv2.imshow('Xavier', xavier_image)
+        if tx2_image is not None:
+            cv2.imshow('TX2', tx2_image)
         cv2.waitKey(1)
