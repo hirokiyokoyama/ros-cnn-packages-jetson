edb7fb4199386a65cf111bccfbcaa922a95e132a
Hiroki Yokoyama <h-yokoyama@craft-server.co.jp>
2019-06-03 14:03:37 +0900

    add dynamixel services

--- openpose/launch_controller.sh
+++ openpose/launch_controller.sh
@@ -9,6 +9,7 @@ xhost +
 ros-container -it --name openpose-controller --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
               -v `pwd`/scripts:/catkin_ws/src/openpose_ros/_scripts \
 	      -v `pwd`/data:/data \
+	      -v `pwd`/dynamixel_controllers:/catkin_ws/devel/lib/python3/dist-packages/dynamixel_controllers \
 	      ros-cnn-packages-jetson-openpose \
 	      rosrun openpose_ros controller.py \
 	      image:=/camera/image_rect_color camera_info:=/camera/camera_info
